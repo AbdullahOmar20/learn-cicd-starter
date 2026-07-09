c8244e5a8e48942525bfea87288d429cbd33de18
abdullah <abdullahamer323@gmail.com>
2026-07-09 14:41:17 +0300

    fix formatting

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -6,57 +6,57 @@ import (
 	"testing"
 )
 
-func TestGetApiKey(t *testing.T){
-	Tests := map[string]struct{
+func TestGetApiKey(t *testing.T) {
+	Tests := map[string]struct {
 		input http.Header
-		want struct {
+		want  struct {
 			res string
 			err error
 		}
 	}{
-		"Correct key": {input: http.Header{"Authorization": []string{"ApiKey dkd"}}, 
-			want: struct{
-				res string 
+		"Correct key": {input: http.Header{"Authorization": []string{"ApiKey dkd"}},
+			want: struct {
+				res string
 				err error
 			}{
 				res: "dkd",
 				err: nil,
-			},},
-		"No ApiKey prefix": {input: http.Header{"Authorization": []string{"dkd"}}, 
-			want: struct{
-				res string 
+			}},
+		"No ApiKey prefix": {input: http.Header{"Authorization": []string{"dkd"}},
+			want: struct {
+				res string
 				err error
 			}{
 				res: "",
 				err: errors.New("malformed authorization header"),
-			},},
-		"Malformed ApiKey prefix": {input: http.Header{"Authorization": []string{"apiKey dkd"}}, 
-			want: struct{
-				res string 
+			}},
+		"Malformed ApiKey prefix": {input: http.Header{"Authorization": []string{"apiKey dkd"}},
+			want: struct {
+				res string
 				err error
 			}{
 				res: "",
 				err: errors.New("malformed authorization header"),
-			},},
-		"Malformed authorization header": {input: http.Header{"authorization": []string{"ApiKey dkd"}}, 
-			want: struct{
-				res string 
+			}},
+		"Malformed authorization header": {input: http.Header{"authorization": []string{"ApiKey dkd"}},
+			want: struct {
+				res string
 				err error
 			}{
 				res: "",
 				err: errors.New("no authorization header included"),
-			},},
-		"No authorization header": {input: http.Header{}, 
-			want: struct{
-				res string 
+			}},
+		"No authorization header": {input: http.Header{},
+			want: struct {
+				res string
 				err error
 			}{
 				res: "",
 				err: errors.New("no authorization header included"),
-			},},
+			}},
 	}
 
-	for name, tc := range Tests{
+	for name, tc := range Tests {
 		t.Run(name, func(t *testing.T) {
 			got, err := GetAPIKey(tc.input)
 			if tc.want.err != nil && err == nil {
@@ -65,7 +65,7 @@ func TestGetApiKey(t *testing.T){
 			if tc.want.err == nil && err != nil {
 				t.Fatalf("expected: %v, got error: %v", tc.want.res, err)
 			}
-			if tc.want.res != got{
+			if tc.want.res != got {
 				t.Fatalf("expected: %v, got: %v", tc.want, got)
 			}
 		})
